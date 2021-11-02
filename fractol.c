9ddf6037ac5a9b4e557e453738910fec6abc62f2
Inigo Alvarez echarri <ialvarez@c2r9s2.42madrid.com>
2021-11-02 22:08:42 +0100

    bb

--- fractol.c
+++ fractol.c
@@ -6,30 +6,30 @@
 /*   By: ialvarez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 19:46:14 by ialvarez          #+#    #+#             */
-/*   Updated: 2021/10/25 20:24:50 by ialvarez         ###   ########.fr       */
+/*   Updated: 2021/11/02 22:08:18 by ialvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
-#include <complex.h>
-
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+#include "fractol.h"
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = data->addr + (y * data->line_lenght + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
 
-void	julia_fractol()
+int		key_hook(int keycode, t_vars *vars)
+{
+	vars = ft_calloc(1, sizeof(t_vars));
+	keycode = 0;
+	printf("Hello from key_hook!\n");
+	return (0);
+}
+
+/*
+void	julia()
 {
 	double _Complex	z;
 	float _Complex	c = 2.0f + 2.0f*_Complex_I;
@@ -37,39 +37,47 @@ void	julia_fractol()
 	
 
 }
-
-int	main(void)
+*/
+int	main(int argc, char **argv)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
+	//t_data	img;
+	t_vars	vars;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+	printf("%d\n", argc);
+	if (argc < 2)
+		write (1, "Not enough arguments\n", 21);
+	if (argc > 2)
+		write (1, "Too many arguments\n", 19);
+	if (argc == 2)
+	{
+		if (ft_strncmp(argv[1],  "Julia", 6) != 0 && ft_strncmp(argv[1],
+				   	"Mandelbrot", 11) != 0)
+			write (1, "You have to write 'Julia' or 'Mandelbrot'\n", 42);
+	}
+	if(argc == 2 && !ft_strncmp(argv[1], "Julia", 6))
+	{
+		vars.mlx = mlx_init();
+		vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
+		mlx_key_hook(vars.win, key_hook, &vars);
+		mlx_loop(vars.mlx);
+		/*
+		img.mlx = mlx_init();
+		img.mlx_win = mlx_new_window(img.mlx, 1920, 1080, "Hello world!");
+		img.img = mlx_new_image(img.mlx, 1080, 1080);
+		img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_lenght,
 								&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FFFF00);
-	my_mlx_pixel_put(&img, 5, 6, 0x00FFFF00);
-	my_mlx_pixel_put(&img, 5, 7, 0x00FFFF00);
-	my_mlx_pixel_put(&img, 5, 8, 0x00FFFF00);
-	my_mlx_pixel_put(&img, 5, 9, 0x00FF0000);
-	my_mlx_pixel_put(&img, 5, 10, 0x00FF0000);
-	my_mlx_pixel_put(&img, 5, 11, 0x00FF0000);
-	my_mlx_pixel_put(&img, 5, 12, 0x00FF0000);
-
-
-
-
-
-
-
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
-
+		my_mlx_pixel_put(&img, 200, 50, 0x00FFFF00);
+		my_mlx_pixel_put(&img, 300, 100, 0x00FFFF00);
+		my_mlx_pixel_put(&img, 400, 150, 0x00FFFF00);
+		my_mlx_pixel_put(&img, 500, 200, 0x00FFFF00);
+		my_mlx_pixel_put(&img, 600, 250, 0x00FFFF00);
+		mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, 0, 0);
+		mlx_loop(img.mlx);*/
+	}
 	/*
 	** After creating an image, we can call `mlx_get_data_addr`, we pass
 	** `bits_per_pixel`, `line_length`, and `endian` by reference. These will
 	** then be set accordingly for the *current* data address.
 	*/
+	return (0);
 }
---
+++ fractol.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialvarez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 18:25:22 by ialvarez          #+#    #+#             */
+/*   Updated: 2021/11/02 22:08:20 by ialvarez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+#include <mlx.h>
+#include <complex.h>
+#include <stdio.h>
+#include <math.h>
+#include "libft/libft.h"
+
+
+typedef struct s_data {
+	void	*mlx;
+	void	*mlx_win;
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_lenght;
+	int		endian;
+}				t_data;
+
+typedef struct s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
+
+#endif
